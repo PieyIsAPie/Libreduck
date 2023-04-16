424cde3c51be3d86ab6999830f643f471848e1bc
Jude Pearson-Phillips <foreynator.serverowner@gmail.com>
2023-04-15 20:49:19 -0400

    Beginning of tabs

---
+++ LazyBrowser/Extensions/Tab.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace LazyBrowser.Extensions
+{
+    internal class Tab
+    {
+        public ChromiumWebBrowser chromeBrowser;
+
+        public Tab()
+        {
+            CefSettings settings = new CefSettings();
+            // Initialize cef with the provided settings
+            Cef.Initialize(settings);
+            // Create a browser component
+            chromeBrowser = new ChromiumWebBrowser("http://duckduckgo.com");
+        }
+    }
+}
