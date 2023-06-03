d2950111d165d1e7ee563155460cba7043544317
Jude Pearson-Phillips <foreynator.serverowner@gmail.com>
2023-06-03 19:57:33 -0400

    beginning of massive change and popups

---
+++ LazyBrowser/Global.cs
@@ -0,0 +1,18 @@
+﻿using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyBrowser
+{
+    internal class Global
+    {
+        public CefSettings settings = new CefSettings();
+
+        public Global() {
+            
+        }
+    }
+}
---
+++ LazyBrowser/Popup.cs
@@ -0,0 +1,23 @@
+﻿using CefSharp;
+using LazyBrowser.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LazyBrowser
+{
+    public partial class Popup : Form
+    {
+        public Popup(string url)
+        {
+            InitializeComponent();
+            Cef.Initialize(settings);
+        }
+    }
+}
